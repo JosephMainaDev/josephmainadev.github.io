a8b269fa6ba55a4805ebabf8842277963c8a21a4
jmkimani <mainajkimani@gmail.com>
2023-05-03 15:20:25 +0300

    trigger rebuild

--- src/app/tools/savings/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Savings() {
-  return (
-  <main className="flex min-h-screen flex-col items-center justify-center">
-    <h1 className="text-5xl font-extrabold">Savings Calculator</h1>
-  </main>
-  )
-}
