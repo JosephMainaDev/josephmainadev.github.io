7e2484b7ff1f8ad0fa975a49402c38707feb4dc7
jmkimani <mainajkimani@gmail.com>
2023-06-15 05:44:57 +0300

    add html structure for homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,55 @@ const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <h1 className="text-5xl font-extrabold">Joseph Maina Portfolio</h1>
+    <main>
+      <div className="flex min-h-screen flex-row items-center justify-center">
+        <div>
+          <h1 className="text-5xl font-extrabold">Joseph Maina, a software engineer and educator.</h1>
+          <p>I help turn to your to live online. 100s of open source contributions. React, Tailwind CSS and MongoDB</p>
+          <button>Get in touch</button>
+        </div>
+        <Image
+          src="https://avatars.githubusercontent.com/u/15126598?v=4"
+          alt="Picture of Joseph Maina"
+          width={500}
+          height={500}
+          className="h-10 w-10 md:h-12 md:w-12 lg:h-16 lg:w-16 rounded-full"
+          unoptimized
+        />
+      </div>
+      <div>
+        <h2>Testimonials from customers and people I have worked with.</h2>
+        <div>These will be small cards with comments that scroll from left to right along the bottom of the screen.</div>
+      </div>
+      <div>
+        <h2>Open source contributions that I want to get me a job</h2>
+        <div>Contribution number ONE plus link to the issue, pull request and merge.</div>
+        <div>Contribution number TWO plus link to the issue, pull request and merge.</div>
+        <div>Contribution number THREE plus link to the issue, pull request and merge.</div>
+      </div>
+      <div>
+        <p>Recent posts. Two or three posts? Decide and adjust accordingly.</p>
+        <div>
+          <h2>Image for the post</h2>
+          <p>Text from the image. I have forgotten what it is called lol!</p>
+        </div>
+        <div>
+          <h2>Image for the post</h2>
+          <p>Text from the image. I have forgotten what it is called lol!</p>
+        </div>
+        <div>
+          <h2>Image for the post</h2>
+          <p>Text from the image. I have forgotten what it is called lol!</p>
+        </div>
+      </div>
+      <div>
+        <h2>Portfolio (of projects I have made). Might be 3 of my favourite projects. Link to Portfolio page with more projects?</h2>
+        <div>
+          <div>This will be a screenshot of the project.</div>
+          <div>This will be a small text block giving a few details about the project. The entire card is a link to GitHub where the code is hosted/readme.</div>
+          <div>List of tech used for this project.</div>
+        </div>
+      </div>
     </main>
   )
 }
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,9 +1,21 @@
+import Image from "next/image"
+
 export default function Header() {
   return (
     <nav className="mx-auto max-w-7xl px-6 lg:px-10 border-b-2 border-orange-100">
-      <div className="relative flex h-16 items-center justify-between">
-        <div>Joseph Maina</div>
-        <div>
+      <div className="relative flex h-24 items-center justify-between">
+        <div className="flex flex-row items-center gap-5">
+          <Image
+            src="https://avatars.githubusercontent.com/u/15126598?v=4"
+            alt="Picture of Joseph Maina"
+            width={100}
+            height={100}
+            className="h-10 w-10 md:h-12 md:w-12 lg:h-16 lg:w-16 rounded-full"
+            unoptimized
+          />
+          <div className="font-medium text-3xl">Joseph Maina</div>
+        </div>
+        <div className="flex flex-row gap-4">
           <span>Projects</span>
           <span>Contact</span>
         </div>
