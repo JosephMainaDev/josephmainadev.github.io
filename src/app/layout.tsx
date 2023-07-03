b39f9ba0da79d9219e92b7342cd84bcaf87cb889
jmkimani <mainajkimani@gmail.com>
2023-07-03 06:39:46 +0300

    Set Inter font globally

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
 import './globals.css'
+import { Inter } from 'next/font/google'
 import Header from '@/components/header'
 import Footer from '@/components/footer'
+
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export const metadata = {
   title: 'Portfolio for Joseph Maina',
@@ -13,7 +19,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.className}>
       <body>
         <Header />
         {children}
